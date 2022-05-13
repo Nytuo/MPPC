439b38f90fdc0e47447748f7a6e831d1a91f56ac
Arnaud BEUX <34894524+Nytuo@users.noreply.github.com>
2022-05-13 20:40:53 +0200

    changing for export

--- js/nrey.js
+++ js/nrey.js
@@ -3,17 +3,19 @@ fetch(getlistURL).then((response) => {
 	return response.json();
 }).then((data) => {
 	var urls = data
-	document.cookie = "userID=252299";
-	var AllOthers = []
-	setTimeout(() => {
-		console.log("here")
+	var ID = document.createElement("p");
+	ID.innerHTML = "ID: " + document.querySelector(".userpicture").src.match(/25[0-9]{4}/g);
+	ID.style.backgroundColor = "rgba(0,0,0,0.5)";
+	ID.style.color = "white";
+	ID.style.position = "fixed";
+	ID.style.top = "97vh";
+	ID.style.left = "95vw";
+	ID.style.zIndex = "9999";
+	document.body.appendChild(ID);
 		document.querySelectorAll('.userpicture').forEach(function (el) {
-			let currentID = el.src.match(/25[0-9]{4}/g)
-			console.log(currentID)
 			if (!el.classList.contains("reyged")) {
-				console.log(urls[currentID])
+				let currentID = el.src.match(/25[0-9]{4}/g)
 				if (urls[currentID] != undefined) {
-
 					var image = urls[currentID]
 					var width = el.offsetWidth;
 					var height = el.offsetHeight;
@@ -21,29 +23,7 @@ fetch(getlistURL).then((response) => {
 					el.setAttribute("src", image);
 					el.style.width = width + "px";
 					el.style.height = height + "px";
-
 				}
 			}
-
-
-
-
 		});
-	}, 1000)
-
-	function getCookie(cname) {
-		let name = cname + "=";
-		let decodedCookie = decodeURIComponent(document.cookie);
-		let ca = decodedCookie.split(';');
-		for (let i = 0; i < ca.length; i++) {
-			let c = ca[i];
-			while (c.charAt(0) == ' ') {
-				c = c.substring(1);
-			}
-			if (c.indexOf(name) == 0) {
-				return c.substring(name.length, c.length);
-			}
-		}
-		return "";
-	}
 });
